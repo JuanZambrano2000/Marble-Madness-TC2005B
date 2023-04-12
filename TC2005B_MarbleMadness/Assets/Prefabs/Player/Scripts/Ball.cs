376e83bc95ccde9a1c81739737ac1fe2ff14bfee
saulcastac <104108980+saulcastac@users.noreply.github.com>
2023-04-12 16:46:48 -0600

    Modificación de movimientos de la bola, laberinto detallado y ajustes de cámara

--- TC2005B_MarbleMadness/Assets/Prefabs/Player/Scripts/Ball.cs
+++ TC2005B_MarbleMadness/Assets/Prefabs/Player/Scripts/Ball.cs
@@ -5,10 +5,10 @@ namespace UnityStandardAssets.Vehicles.Ball
 {
     public class Ball : MonoBehaviour
     {
-        [SerializeField] private float m_MovePower = 5; // The force added to the ball to move it.
+        [SerializeField] private float m_MovePower = 1; // The force added to the ball to move it.
         [SerializeField] private bool m_UseTorque = true; // Whether or not to use torque to move the ball.
-        [SerializeField] private float m_MaxAngularVelocity = 25; // The maximum velocity the ball can rotate at.
-        [SerializeField] private float m_JumpPower = 2; // The force added to the ball when it jumps.
+        [SerializeField] private float m_MaxAngularVelocity = 1; // The maximum velocity the ball can rotate at.
+        [SerializeField] private float m_JumpPower = 1; // The force added to the ball when it jumps.
 
         private const float k_GroundRayLength = 1f; // The length of the ray to check if the ball is grounded.
         private Rigidbody m_Rigidbody;
